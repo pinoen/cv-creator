bcc5ea739045164813b59b3b2d7456af16395699
Emilio Pino <pinoen@yahoo.com.ar>
2023-01-31 17:41:50 -0300

    complete basic header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
+import EmailIcon from '@mui/icons-material/Email';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 const Header = () => {
 
@@ -12,10 +17,28 @@ const Header = () => {
   return (
     <div className='header-container'
       onMouseOver={handleMouseOver}
-      onMouseOut={handleMouseOut}
-
-    >Header
+      onMouseOut={handleMouseOut}>
       <button className='edit-header'>Edit</button>
+
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+
+        <div>
+          <h1>EMILIO NICOLAS PINO</h1>
+          <h3>Full Stack Developer</h3>
+        </div>
+        <img id='profile-img' src='https://avatars.githubusercontent.com/u/91059020?v=4' alt='profile'></img>
+
+      </div>
+
+      <div className='contact-data'>
+        <PhoneAndroidIcon color='action' /><p>+54 9 299 4184677</p>
+        <EmailIcon color='action' /><a href='mailto:pinoen@yahoo.com.ar'>pinoen@yahoo.com.ar</a>
+        <LocationOnIcon color='action' /><p>Mendoza, Argentina</p>
+        <GitHubIcon color='action' /><a href='https://github.com/pinoen' target={'_blank'} rel="noreferrer">github.com/pinoen</a>
+        <LinkedInIcon color='action' /><a href='https://www.linkedin.com/in/emiliopino/' target={'_blank'} rel="noreferrer">linkedin.com/in/emiliopino</a>
+      </div>
+
+
 
     </div>
   )
