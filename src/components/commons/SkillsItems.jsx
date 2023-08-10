df020d471a2eda83b84efba84741ad934c2edcbf
Emilio Pino <pinoen@yahoo.com.ar>
2023-08-10 16:16:17 -0300

    add key prop to each span that hold a skill

--- src/components/commons/SkillsItems.jsx
+++ src/components/commons/SkillsItems.jsx
@@ -7,49 +7,49 @@ const SkillsItems = ({ client, server, testing, devops, tools, soft, languages }
       <h4 className='.skill-section-title'>Client-side</h4>
       <div className='skill-section-box'>
         {client.toUpperCase().split(',').map(skill => (
-          <span>{skill}</span>
+          <span key={skill}>{skill}</span>
         ))}
       </div>
 
       <h4 className='.skill-section-title'>Server-side</h4>
       <div className='skill-section-box'>
         {server.toUpperCase().split(',').map(skill => (
-          <span>{skill}</span>
+          <span key={skill}>{skill}</span>
         ))}
       </div>
 
       <h4 className='.skill-section-title'>Testing</h4>
       <div className='skill-section-box'>
         {testing.toUpperCase().split(',').map(skill => (
-          <span>{skill}</span>
+          <span key={skill}>{skill}</span>
         ))}
       </div>
 
       <h4 className='.skill-section-title'>DevOps</h4>
       <div className='skill-section-box'>
         {devops.toUpperCase().split(',').map(skill => (
-          <span>{skill}</span>
+          <span key={skill}>{skill}</span>
         ))}
       </div>
 
       <h4 className='.skill-section-title'>Tools</h4>
       <div className='skill-section-box'>
         {tools.toUpperCase().split(',').map(skill => (
-          <span>{skill}</span>
+          <span key={skill}>{skill}</span>
         ))}
       </div>
 
       <h4 className='.skill-section-title'>Soft-skills</h4>
       <div className='skill-section-box'>
         {soft.toUpperCase().split(',').map(skill => (
-          <span>{skill}</span>
+          <span key={skill}>{skill}</span>
         ))}
       </div>
 
       <h4 className='.skill-section-title'>Languages</h4>
       <div className='skill-section-box'>
         {languages.toUpperCase().split(',').map(skill => (
-          <span>{skill}</span>
+          <span key={skill}>{skill}</span>
         ))}
       </div>
     </>
